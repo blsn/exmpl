165ffa5f15db9956b739f6e40cbaa4dd67b0637a
blsn <blsn2000@gmail.com>
2022-01-25 00:36:12 +0200

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth', ['except' => ['index', 'about']]);
+    }
+
+    public function index() {
+        $title = 'Homepage';
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about() {
+        $title = 'About us';
+        return view('pages.about')->with('title', $title);
+    }
+
+    public function services() {
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Modem Solutions', 'Multiservice Switches', 'Ethernet Access']
+        );
+        return view('pages.Services')->with($data);
+    }
+}
